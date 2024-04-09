2950f93eb2e6b6f249cb8e3b1cfe0e858bdbecb1
JoGMG <joattah3@gmail.com>
2024-04-09 22:55:23 +0100

    added api stats

--- api/views/index.py
+++ api/views/index.py
@@ -3,6 +3,9 @@
 from flask import jsonify
 from api.views import app_views
 from models import storage
+from models.movie import Movie
+from models.genre import Genre
+from models.cast import Cast
 from models.user import User
 from models.review import Review
 
@@ -16,9 +19,15 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """ Retrieves count of User and Review instances. """
+    movies = storage.count(Movie)
+    genres = storage.count(Genre)
+    cast = storage.count(Cast)
     users = storage.count(User)
     reviews = storage.count(Review)
     return jsonify({
+        "movies": movies,
+        "genre": genres,
+        "cast": cast,
         "users": users,
         "reviews": reviews
     })
